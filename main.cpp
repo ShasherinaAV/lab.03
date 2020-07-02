99a87874c24c6e761a25165319ffdf3dfe567a3b
Shasherina Alexandra <shasherina.alexandra@gmail.com>
2020-07-02 10:46:05 +0300

    Загрузка файла по сети

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <curl/curl.h>
+#include <string>
 #include "histogram.h"
 #include "svg.h"
+
+
 
 
 using namespace std;
@@ -113,8 +116,14 @@ main(int argc, char* argv[])
 {
     if (argc > 1)
     {
-        for(int i=0; i<argc; i++)
-            cout<<"argv["<<i<<"] = "<<argv[i]<<'\n';
+        CURL *curl = curl_easy_init();
+        if(curl)
+        {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+        }
         return 0;
     }
 
