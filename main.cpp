0b824a5b83acd8dbcb50dc2757eaeb494ba4ab55
Shasherina Alexandra <shasherina.alexandra@gmail.com>
2020-07-03 00:22:10 +0300

    Сохранение данных из сети в буфер

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <curl/curl.h>
+#include <sstream>
 #include <string>
 #include "histogram.h"
 #include "svg.h"
@@ -23,8 +24,7 @@ vector<double> input_numbers(istream& in, const size_t count)
 
 }
 
-Input
-read_input(istream& in, bool prompt)
+Input read_input(istream& in, bool prompt)
 {
     Input data;
     if(prompt)
@@ -110,35 +110,52 @@ void show_histogram_text(vector<size_t> bins)
     }
 
 }
-
-int
-main(int argc, char* argv[])
+size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx)
 {
+    const size_t data_size= item_size*item_count;
+    const char* new_items = reinterpret_cast<const char*>(items);
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(new_items, data_size);
+    return data_size;
+}
+
+Input download(const string& address)
+{
+    stringstream buffer;
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    CURL *curl = curl_easy_init();
+    if(curl)
+    {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK)
+        {
+            cout << curl_easy_strerror(res) << endl;
+            exit(1);
+        }
+    }
+    curl_easy_cleanup(curl);
+    return read_input(buffer, false);
+}
+
+int main(int argc, char* argv[])
+{
+    Input input;
     if (argc > 1)
     {
-        CURL *curl = curl_easy_init();
-        if(curl)
-        {
-            CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-            res = curl_easy_perform(curl);
-            res = CURLE_FAILED_INIT;
-            if (res != CURLE_OK)
-            {
-                cout << curl_easy_strerror(res) << endl;
-                exit(1);
-            }
-            curl_easy_cleanup(curl);
-        }
-        return 0;
+        input = download(argv[1]);
+    }
+    else
+    {
+        input = read_input(cin, true);
     }
 
-    curl_global_init(CURL_GLOBAL_ALL);
-    const auto input = read_input(cin, true);
+const auto bins = make_histogram(input);
+show_histogram_svg(bins);
 
-    const auto bins = make_histogram(input);
-
-    show_histogram_svg(bins);
-
-    return 0;
+return 0;
 }
